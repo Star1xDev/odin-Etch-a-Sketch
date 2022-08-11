bcb68bd836ef923bdfc3e501ead753f6462c80c9
Star1xDev <abdastar2@gmail.com>
2022-08-11 17:13:27 +0100

    added function createGrid
    takes number and creates number*number div inside .container

---
+++ app.js
@@ -0,0 +1,32 @@
+// get container reference and height and width
+const containerDiv = document.querySelector(".container");
+const containerWidth = containerDiv.offsetWidth;
+const containerHeight = containerDiv.offsetHeight;
+
+// number of div per side for the grid
+let gridNumber = 16;
+
+// calculate the width and height for the divs
+let squareWidth = containerWidth / gridNumber;
+let squareHeight = containerHeight / gridNumber;
+
+// function to  create square divs with the height and width from before and appending to container //
+function createGrid(gridNumber){
+  let i = 0;
+  while (i < gridNumber * gridNumber) {
+    let squareDiv = document.createElement("div");
+    squareDiv.classList.add("square");
+    squareDiv.style.width = squareWidth + "px";
+    squareDiv.style.height = squareHeight + "px";
+    containerDiv.appendChild(squareDiv);
+    i++;
+  }
+}
+
+createGrid(gridNumber);
+
+
+
+
+
+
